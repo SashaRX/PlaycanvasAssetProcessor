55a0461b91e66ec2fda199c2f58439aef4c428ce
SashaRX <SashaRX@ya.ru>
2025-11-18 03:50:57 +0100

    Fix test dependencies

--- AssetProcessor.Tests/Services/ProjectAssetServiceTests.cs
+++ AssetProcessor.Tests/Services/ProjectAssetServiceTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using AssetProcessor.Resources;
 using AssetProcessor.Services;
 using AssetProcessor.Services.Models;
 using Newtonsoft.Json.Linq;
